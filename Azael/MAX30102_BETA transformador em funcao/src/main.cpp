e6340f4e12ad4b80d7ea929e00de218b078aebe9
Zagreus <99051910+Zagreus1447@users.noreply.github.com>
2023-10-26 08:58:29 -0300

    tentei arrumar o problema do spo2 e da beatavg
    spo2 tava retornando 1 e beatavg 0 tentei arrumar esse dois problema, mas n sei pq ele ta passando 90% de oxigenação sem o dedo ele para ele fazer a verificação antes talvez ( o bpm tem de passar de um quantidade para poder enviar.

--- Azael/MAX30102_BETA transformador em funcao/src/main.cpp
+++ Azael/MAX30102_BETA transformador em funcao/src/main.cpp
@@ -54,172 +54,84 @@ void setup()
 
 double getSpo2() // FUNCAO RETORNA O SPO2
 {
-  long irValue = particleSensor.getIR(); // Reading the IR value it will permit us to know if there's a finger on the sensor or not
-  // Check if a finger is placed
-  if (irValue > FINGER_ON)
+  while (beatAvg < 30)
   {
-
-    // Check for a heartbeat and measure heart rate
-    if (checkForBeat(irValue) == true)
+    long irValue = particleSensor.getIR(); // Reading the IR value it will permit us to know if there's a finger on the sensor or not
+    // Check if a finger is placed
+    if (irValue > FINGER_ON)
     {
+
+      // Check for a heartbeat and measure heart rate
+      if (checkForBeat(irValue) == true)
+      {
+        sendBeat = beatAvg;
+        // Serial.println(" BPM"); // Display heart rate value
+        //  Display blood oxygen value
+        if (beatAvg > 30)
+        {
+          return ESpO2;
+        }
+
+        sendBeat = beatAvg;
+        long delta = millis() - lastBeat; // Calculate heart rate difference
+        lastBeat = millis();
+        beatsPerMinute = 60 / (delta / 1000.0); // Calculate average heart rate
+        if (beatsPerMinute < 255 && beatsPerMinute > 20)
+        {
+          // The heartbeat must be between 20-255
+          rates[rateSpot++] = (byte)beatsPerMinute; // Array to store heartbeat values
+          rateSpot %= RATE_SIZE;
+          beatAvg = 0; // Calculate average
+          for (byte x = 0; x < RATE_SIZE; x++)
+            beatAvg += rates[x];
+          beatAvg /= RATE_SIZE;
+        }
+      }
+
+      // Medir o oxigênio no sangue
+      uint32_t ir, red;
+      double fred, fir;
+      particleSensor.check(); // Check the sensor, read up to 3 samples
+      if (particleSensor.available())
+      {
+        i++;
+        ir = particleSensor.getFIFOIR();   // Ler infravermelho
+        red = particleSensor.getFIFORed(); // Leia a luz vermelha
+        // Serial.println("red=" + String(red) + ",IR=" + String(ir) + ",i=" + String(i));
+        fir = (double)ir;                                      // Convert double
+        fred = (double)red;                                    // Convert double
+        aveir = aveir * frate + (double)ir * (1.0 - frate);    // average IR level by low pass filter
+        avered = avered * frate + (double)red * (1.0 - frate); // average red level by low pass filter
+        sumirrms += (fir - aveir) * (fir - aveir);             // square sum of alternate component of IR level
+        sumredrms += (fred - avered) * (fred - avered);        // square sum of alternate component of red level
+
+        if ((i % Num) == 0)
+        {
+          double R = (sqrt(sumirrms) / aveir) / (sqrt(sumredrms) / avered);
+          SpO2 = -23.3 * (R - 0.4) + 100;
+          ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // low pass filter
+          if (ESpO2 <= MINIMUM_SPO2)
+            ESpO2 = MINIMUM_SPO2; // indicator for finger detached
+          if (ESpO2 > 100)
+            ESpO2 = 99.9;
+          // Serial.print(",SPO2="); Serial.println(ESpO2);
+          sumredrms = 0.0;
+          sumirrms = 0.0;
+          SpO2 = 0;
+          i = 0;
+        }
+        particleSensor.nextSample(); // We're finished with this sample so move to next sample
+      }
+
+      // exibir dados em serial
       sendBeat = beatAvg;
-      // Serial.println(" BPM"); // Display heart rate value
-      //  Display blood oxygen value
+      // Exibir o valor do oxigênio no sangue para evitar medições incorretas. É necessário exibir o oxigênio no sangue quando o batimento cardíaco excede 30
       if (beatAvg > 30)
-      {
-        return ESpO2;
-      }
+        sendBeat = beatAvg;
       else
-        ESpO2 = 0;
-
-      sendBeat = beatAvg;
-      long delta = millis() - lastBeat; // Calculate heart rate difference
-      lastBeat = millis();
-      beatsPerMinute = 60 / (delta / 1000.0); // Calculate average heart rate
-      if (beatsPerMinute < 255 && beatsPerMinute > 20)
-      {
-        // The heartbeat must be between 20-255
-        rates[rateSpot++] = (byte)beatsPerMinute; // Array to store heartbeat values
-        rateSpot %= RATE_SIZE;
-        beatAvg = 0; // Calculate average
-        for (byte x = 0; x < RATE_SIZE; x++)
-          beatAvg += rates[x];
-        beatAvg /= RATE_SIZE;
-      }
+        return ESpO2;
     }
-
-    // Medir o oxigênio no sangue
-    uint32_t ir, red;
-    double fred, fir;
-    particleSensor.check(); // Check the sensor, read up to 3 samples
-    if (particleSensor.available())
-    {
-      i++;
-      ir = particleSensor.getFIFOIR();   // Ler infravermelho
-      red = particleSensor.getFIFORed(); // Leia a luz vermelha
-      // Serial.println("red=" + String(red) + ",IR=" + String(ir) + ",i=" + String(i));
-      fir = (double)ir;                                      // Convert double
-      fred = (double)red;                                    // Convert double
-      aveir = aveir * frate + (double)ir * (1.0 - frate);    // average IR level by low pass filter
-      avered = avered * frate + (double)red * (1.0 - frate); // average red level by low pass filter
-      sumirrms += (fir - aveir) * (fir - aveir);             // square sum of alternate component of IR level
-      sumredrms += (fred - avered) * (fred - avered);        // square sum of alternate component of red level
-
-      if ((i % Num) == 0)
-      {
-        double R = (sqrt(sumirrms) / aveir) / (sqrt(sumredrms) / avered);
-        SpO2 = -23.3 * (R - 0.4) + 100;
-        ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // low pass filter
-        if (ESpO2 <= MINIMUM_SPO2)
-          ESpO2 = MINIMUM_SPO2; // indicator for finger detached
-        if (ESpO2 > 100)
-          ESpO2 = 99.9;
-        // Serial.print(",SPO2="); Serial.println(ESpO2);
-        sumredrms = 0.0;
-        sumirrms = 0.0;
-        SpO2 = 0;
-        i = 0;
-      }
-      particleSensor.nextSample(); // We're finished with this sample so move to next sample
-    }
-
-    // exibir dados em serial
-    sendBeat = beatAvg;
-    // Exibir o valor do oxigênio no sangue para evitar medições incorretas. É necessário exibir o oxigênio no sangue quando o batimento cardíaco excede 30
-    if (beatAvg > 30)
-      sendBeat = beatAvg;
-    else
-      return ESpO2;
-  }
-  // Nenhum dedo detectado, limpe todos os dados e conteúdo da tela "Finger Please"
-  else
-  {
-    // Limpar dados de batimentos cardíacos
-    for (byte rx = 0; rx < RATE_SIZE; rx++)
-      rates[rx] = 0;
-    beatAvg = 0;
-    rateSpot = 0;
-    lastBeat = 0;
-    // Limpar dados de oxigênio no sangue
-    avered = 0;
-    aveir = 0;
-    sumirrms = 0;
-    sumredrms = 0;
-    SpO2 = 0;
-    ESpO2 = 90.0;
-    // Exibição Finger Please
-    // Serial.println("Dedo não encontrado ");
-  }
-  return 1;
-}
-
-int getBeatAvg() // FUNCAO RETORNA O BeatAvg
-{
-  long irValue = particleSensor.getIR(); // Reading the IR value it will permit us to know if there's a finger on the sensor or not
-  // Check if a finger is placed
-  if (irValue > FINGER_ON)
-  {
-
-    // Check for a heartbeat and measure heart rate
-    if (checkForBeat(irValue) == true)
-    {
-      // Serial.println(" BPM"); // Display heart rate value
-      //  Display blood oxygen value
-
-      sendBeat = beatAvg;
-      long delta = millis() - lastBeat; // Calculate heart rate difference
-      lastBeat = millis();
-      beatsPerMinute = 60 / (delta / 1000.0); // Calculate average heart rate
-      if (beatsPerMinute < 255 && beatsPerMinute > 20)
-      {
-        // The heartbeat must be between 20-255
-        rates[rateSpot++] = (byte)beatsPerMinute; // Array to store heartbeat values
-        rateSpot %= RATE_SIZE;
-        beatAvg = 0; // Calculate average
-        for (byte x = 0; x < RATE_SIZE; x++)
-          beatAvg += rates[x];
-        beatAvg /= RATE_SIZE;
-      }
-    }
-
-    // Medir o oxigênio no sangue
-    uint32_t ir, red;
-    double fred, fir;
-    particleSensor.check(); // Check the sensor, read up to 3 samples
-    if (particleSensor.available())
-    {
-      i++;
-      ir = particleSensor.getFIFOIR();   // Ler infravermelho
-      red = particleSensor.getFIFORed(); // Leia a luz vermelha
-      // Serial.println("red=" + String(red) + ",IR=" + String(ir) + ",i=" + String(i));
-      fir = (double)ir;                                      // Convert double
-      fred = (double)red;                                    // Convert double
-      aveir = aveir * frate + (double)ir * (1.0 - frate);    // average IR level by low pass filter
-      avered = avered * frate + (double)red * (1.0 - frate); // average red level by low pass filter
-      sumirrms += (fir - aveir) * (fir - aveir);             // square sum of alternate component of IR level
-      sumredrms += (fred - avered) * (fred - avered);        // square sum of alternate component of red level
-
-      if ((i % Num) == 0)
-      {
-        double R = (sqrt(sumirrms) / aveir) / (sqrt(sumredrms) / avered);
-        SpO2 = -23.3 * (R - 0.4) + 100;
-        ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // low pass filter
-        if (ESpO2 <= MINIMUM_SPO2)
-          ESpO2 = MINIMUM_SPO2; // indicator for finger detached
-        if (ESpO2 > 100)
-          ESpO2 = 99.9;
-        // Serial.print(",SPO2="); Serial.println(ESpO2);
-        sumredrms = 0.0;
-        sumirrms = 0.0;
-        SpO2 = 0;
-        i = 0;
-      }
-      particleSensor.nextSample(); // We're finished with this sample so move to next sample
-    }
-
-    if (beatAvg > 30)
-      return beatAvg;
-    // Nenhum dedo detectado, limpe todos os dados
+    // Nenhum dedo detectado, limpe todos os dados e conteúdo da tela "Finger Please"
     else
     {
       // Limpar dados de batimentos cardíacos
@@ -235,9 +147,101 @@ int getBeatAvg() // FUNCAO RETORNA O BeatAvg
       sumredrms = 0;
       SpO2 = 0;
       ESpO2 = 90.0;
+      // Exibição Finger Please
+      // Serial.println("Dedo não encontrado ");
     }
+    // return 1; //erro
   }
-  return 1;
+}
+
+int getBeatAvg() // FUNCAO RETORNA O BeatAvg
+{
+  while (beatAvg < 30)
+  {
+    long irValue = particleSensor.getIR(); // Reading the IR value it will permit us to know if there's a finger on the sensor or not
+    // Check if a finger is placed
+    if (irValue > FINGER_ON)
+    {
+
+      // Check for a heartbeat and measure heart rate
+      if (checkForBeat(irValue) == true)
+      {
+        // Serial.println(" BPM"); // Display heart rate value
+        //  Display blood oxygen value
+
+        sendBeat = beatAvg;
+        long delta = millis() - lastBeat; // Calculate heart rate difference
+        lastBeat = millis();
+        beatsPerMinute = 60 / (delta / 1000.0); // Calculate average heart rate
+        if (beatsPerMinute < 255 && beatsPerMinute > 20)
+        {
+          // The heartbeat must be between 20-255
+          rates[rateSpot++] = (byte)beatsPerMinute; // Array to store heartbeat values
+          rateSpot %= RATE_SIZE;
+          beatAvg = 0; // Calculate average
+          for (byte x = 0; x < RATE_SIZE; x++)
+            beatAvg += rates[x];
+          beatAvg /= RATE_SIZE;
+        }
+      }
+
+      // Medir o oxigênio no sangue
+      uint32_t ir, red;
+      double fred, fir;
+      particleSensor.check(); // Check the sensor, read up to 3 samples
+      if (particleSensor.available())
+      {
+        i++;
+        ir = particleSensor.getFIFOIR();   // Ler infravermelho
+        red = particleSensor.getFIFORed(); // Leia a luz vermelha
+        // Serial.println("red=" + String(red) + ",IR=" + String(ir) + ",i=" + String(i));
+        fir = (double)ir;                                      // Convert double
+        fred = (double)red;                                    // Convert double
+        aveir = aveir * frate + (double)ir * (1.0 - frate);    // average IR level by low pass filter
+        avered = avered * frate + (double)red * (1.0 - frate); // average red level by low pass filter
+        sumirrms += (fir - aveir) * (fir - aveir);             // square sum of alternate component of IR level
+        sumredrms += (fred - avered) * (fred - avered);        // square sum of alternate component of red level
+
+        if ((i % Num) == 0)
+        {
+          double R = (sqrt(sumirrms) / aveir) / (sqrt(sumredrms) / avered);
+          SpO2 = -23.3 * (R - 0.4) + 100;
+          ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2; // low pass filter
+          if (ESpO2 <= MINIMUM_SPO2)
+            ESpO2 = MINIMUM_SPO2; // indicator for finger detached
+          if (ESpO2 > 100)
+            ESpO2 = 99.9;
+          // Serial.print(",SPO2="); Serial.println(ESpO2);
+          sumredrms = 0.0;
+          sumirrms = 0.0;
+          SpO2 = 0;
+          i = 0;
+        }
+        particleSensor.nextSample(); // We're finished with this sample so move to next sample
+      }
+
+      if (beatAvg > 30)
+        return beatAvg;
+      // Nenhum dedo detectado, limpe todos os dados
+      else
+      {
+        // Limpar dados de batimentos cardíacos
+        for (byte rx = 0; rx < RATE_SIZE; rx++)
+          rates[rx] = 0;
+        beatAvg = 0;
+        rateSpot = 0;
+        lastBeat = 0;
+        // Limpar dados de oxigênio no sangue
+        avered = 0;
+        aveir = 0;
+        sumirrms = 0;
+        sumredrms = 0;
+        SpO2 = 0;
+        ESpO2 = 90.0;
+      }
+    }
+    // return 1; //erro = 1
+  }
 }
 
 void loop()
